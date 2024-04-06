c53b043fb51e6d1130e7f632cb61e524823a5e70
Sayam Badoni <sayambadoni2@gmail.com>
2024-04-07 00:05:47 +0530

    routes created.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from '../../client/pages/Home';
+import About from '../../client/pages/About';
+import Signin from '../../client/pages/Signin';
+import Signup from '../../client/pages/Signup';
+import Dashboard from '../../client/pages/Dashboard';
+import Projects from '../../client/pages/Projects';
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/sign-in' element={<Signin />}/>
+        <Route path='/sign-up' element={<Signup />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/projects' element={<Projects />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
