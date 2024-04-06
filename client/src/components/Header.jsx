9f1b9466efabe9ad1a9cb51e76903624c294b7d5
Sayam Badoni <sayambadoni2@gmail.com>
2024-04-07 01:29:06 +0530

    create and run api

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ function Header() {
                 <FaMoon />
             </Button>
             <Link>
-              <Button gradientDuoTone='purpleToBlue'>
+              <Button gradientDuoTone='purpleToBlue' outline>
                 Sign In
               </Button>
             </Link>
