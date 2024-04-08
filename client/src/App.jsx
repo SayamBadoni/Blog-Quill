1f267ba2e1d5d8c37895054e2ee9d74f4ef1d076
Sayam Badoni <sayambadoni2@gmail.com>
2024-04-09 00:38:39 +0530

    create and add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Dashboard';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/dashboard' element={<Dashboard />}/>
         <Route path='/projects' element={<Projects />}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
